1b7726d935f8731324a5138c6bc1d9756b5ceedc
mkiszkur <54988661+mkiszkur@users.noreply.github.com>
2024-04-26 23:44:34 -0300

    data

---
+++ preprocess_data/generar_indicadores.py
@@ -0,0 +1,66 @@
+import os
+import pandas as pd
+import openpyxl
+import xlrd
+import logging
+
+# Configura el logging para capturar errores
+def setup_logging(log_path):
+    logging.basicConfig(filename=log_path, filemode='w', level=logging.ERROR)
+
+# Función para procesar archivos Excel y agregar datos al CSV usando openpyxl
+def add_data_to_csv_xlsx(excel_file, csv_file):
+    try:
+        # Carga la solapa específica
+        workbook = openpyxl.load_workbook(excel_file, data_only=True)
+        sheet = workbook['Metadata - Indicators']
+        data = {
+            'INDICATOR_CODE': sheet['A2'].value,
+            'FILE_NAME': excel_file,
+            'INDICATOR_NAME': sheet['B2'].value,
+            'SOURCE_NOTE': sheet['C2'].value,
+            'SOURCE_ORGANIZATION': sheet['D2'].value
+        }
+        df = pd.DataFrame([data])
+        df.to_csv(csv_file, mode='a', header=False, index=False)
+    except Exception as e:
+        logging.error(f'Error processing file {excel_file}: {e}')
+
+# Función para procesar archivos Excel y agregar datos al CSV usando xlrd
+def add_data_to_csv_xls(excel_file, csv_file):
+    try:
+        # Abre el archivo xls
+        workbook = xlrd.open_workbook(excel_file)
+        sheet = workbook.sheet_by_name('Metadata - Indicators')
+        data = {
+            'INDICATOR_CODE': sheet.cell(1, 0).value,
+            'FILE_NAME': excel_file,
+            'INDICATOR_NAME': sheet.cell(1, 1).value,
+            'SOURCE_NOTE': sheet.cell(1, 2).value,
+            'SOURCE_ORGANIZATION': sheet.cell(1, 3).value
+        }
+        df = pd.DataFrame([data])
+        df.to_csv(csv_file, mode='a', header=False, index=False)
+    except Exception as e:
+        logging.error(f'Error processing file {excel_file}: {e}')
+
+# Función principal para iterar sobre los archivos Excel en un directorio
+def process_folder(directory, csv_file):
+    log_path = csv_file.replace('.csv', '.log')
+    setup_logging(log_path)
+    
+    for file in os.listdir(directory):
+        if file.endswith('.xlsx'):
+            excel_file = os.path.join(directory, file)
+            add_data_to_csv_xlsx(excel_file, csv_file)
+        elif file.endswith('.xls'):
+            excel_file = os.path.join(directory, file)
+            add_data_to_csv_xls(excel_file, csv_file)
+        #print (excel_file)
+# Reemplaza con el directorio de tus archivos y el nombre de tu archivo CSV
+
+directory_path = '/Users/miguelkiszkurno/Documents/TT1/data'
+csv_file_path = '/Users/miguelkiszkurno/Documents/TT1/indicadores.csv'
+
+# Inicia el procesamiento
+process_folder(directory_path, csv_file_path)
--- scrapper/Selenium.py
+++ scrapper/Selenium.py
@@ -6,12 +6,14 @@ import csv  # Importa la biblioteca csv
 import pandas as pd
 import os
 import re
-#Genera un csv con losndatasets que hay en la pagina de world bank
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+#Genera un csv con los ndatasets que hay en la pagina de world bank
 # Hay que hacer un postprocessing porque deja algunos archivos con basura
 # Ejemplo de uso del browse_datasets
 #p = 'https://data.worldbank.org/indicator?tab=all'
 #browse_datasets(p)    
-
 def browse_datasets(p):
     driver = webdriver.Chrome()
     driver.get(p)
@@ -61,8 +63,6 @@ def obtener_dataset(p):
 
 
 #Descarga los datasets a la carpeta downloads. 
-
-
 def download_from_excel_deprecated(file_path):
     # Leer el archivo Excel
     df = pd.read_csv(file_path, delimiter='|')
@@ -89,16 +89,17 @@ def download_from_excel_deprecated(file_path):
     # Cerrar el navegador al final de todas las descargas
     driver.quit()
 
+#Download de todos los archivos de datos que figuran en el archivo de input
 def download_from_excel(file_path):
     # Leer el archivo Excel
-    df = pd.read_csv(file_path, delimiter='|')
+    df = pd.read_csv(file_path, delimiter=',')
     print (df.columns)
     # Configurar el navegador
     driver = webdriver.Chrome()
 
     # Recorrer las filas del DataFrame
     for index, row in df.iterrows():
-        if row['status'] != 'descargado':  # Verificar si el status no es 'descargado'
+        if row['estado'] != 'descargado':  # Verificar si el status no es 'descargado'
             url = row['direccion']
             try:
                 driver.get(url)
@@ -112,7 +113,7 @@ def download_from_excel(file_path):
                 time.sleep(1)  # Aumentado a 10 segundos para dar más tiempo de respuesta
 
                 # Actualizar el status en el DataFrame
-                df.at[index, 'status'] = 'descargado'
+                df.at[index, 'estado'] = 'descargado'
             except Exception as e:
                 print(f"Error en la descarga desde {url}: {e}")
 
@@ -120,12 +121,98 @@ def download_from_excel(file_path):
     driver.quit()
     
     # Guardar los cambios en el archivo CSV
-    df.to_csv(file_path, index=False, sep='|')
+    df.to_csv(file_path, index=False, sep=',')
 
 
 
 #Ejemplo de uso
-download_from_excel('/Users/miguelkiszkurno/Documents/TT1/Data/_datasets_2.csv')
+#download_from_excel('/Users/miguelkiszkurno/Documents/TT1/Data/_datasets_2.csv')
+
+#Recorre las secciones de lapaguna en busca de los arvhivos. La deprecamos porque me quedo mejor la funcion que recorre los 
+#uls
+
+def encontrar_secciones(p):
+    driver = webdriver.Chrome()
+    driver.get(p)
+    driver.implicitly_wait(0.5)  # Esperar que los elementos estén disponibles
+
+    # Define 'wait' dentro de la función para que esté disponible
+    wait = WebDriverWait(driver, 10)
+    
+
+    # Encuentra todos los elementos <li> que contengan un enlace <a>
+    secciones = driver.find_elements(By.CLASS_NAME, 'nav-item')
+
+    with open('resultados.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # Escribe los encabezados del archivo CSV
+        writer.writerow(['Elemento', 'Subelemento', 'Href del Subelemento'])
+
+        # Recorre todas las secciones encontradas
+        for seccion in secciones:
+            seccion_text = seccion.text
+            print (f"***seccion:{seccion.text}***")
+            #children = seccion.find_elements(By.XPATH, ".//*")  # Selecciona todos los elementos descendientes
+            c = seccion.find_elements(By.XPATH, "./descendant::ul")
+            print(type(c))
+            children = seccion.find_elements(By.XPATH, "./descendant::ul")
+            for child in children:
+                #print(f"Tag: {child.tag_name}, Texto: {child.text}, Atributos: {child.get_attribute('outerHTML')}")
+                #print(f"Tag: {child.tag_name},href:{child.get_attribute('href')}")
+                link_element = child.find_element(By.TAG_NAME, 'a')
+                href_value = link_element.get_attribute('href')
+
+                # Extrae el texto visible del enlace
+                link_text = link_element.text
+
+                writer.writerow([seccion_text, link_text, href_value])
+            # Encuentra el enlace dentro del h3 y haz clic para entrar en la sección
+    # Cierra el navegador
+    driver.quit()
+
+#encontrar_secciones("https://data.worldbank.org/indicator")
 
 
+
+
+def recorrer_datasets(p):
+    driver = webdriver.Chrome()
+    driver.get(p)
+    driver.implicitly_wait(0.5)  # Esperar que los elementos estén disponibles    
+
+    with open('resultados.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # Escribe los encabezados del archivo CSV
+        writer.writerow(['titulo_seccion', 'nombre_dataset', 'codigo_indicador', 'estado', 'direccion'])
+
+        #son 20 secciones
+        for i in range(1,20):
+            #header con el titulo
+            h3 = driver.find_element(By.XPATH, f"//*[@id='main']/div[2]/section[{i}]/h3")
+            titulo_seccion = h3.text
+            
+            #ul con la lista de datasets/indicadores
+            c = driver.find_elements(By.XPATH, f"//*[@id='main']/div[2]/section[{i}]/ul")
+
+            #Busco todos los elementos del ultimo ul (que es el que contiene los datos)
+            children = c[len(c)-1].find_elements(By.XPATH, "./descendant::a")
+
+            for child in children:
+                #me quedo con la url
+                href_value = child.get_attribute('href')
+                nombre_indicador = re.search(r'indicator/(.*?)\?view=chart', href_value).group(1)
+
+                # Extrae el texto visible del enlace
+                
+                titulo_indicador = child.text
+                #print(f"Tag: {child.tag_name}, Texto: {child.text}, Atributos: {child.get_attribute('outerHTML')}")
+                print(f"###Titulo: {titulo_seccion}text: {child.text},href:{child.get_attribute('href')}")
+                
+                writer.writerow([titulo_seccion, nombre_indicador, titulo_indicador, 'pendiente', href_value ])
+    # Cierra el navegador
+    driver.quit()
+
+
+#recorrer_datasets("https://data.worldbank.org/indicator?tab=all")
+download_from_excel('/Users/miguelkiszkurno/Documents/TT1/resultados.csv')
 
