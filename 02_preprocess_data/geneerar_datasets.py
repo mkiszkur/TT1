30bc82d829e29e51dff9faeae48d5ad08f5e111f
mkiszkur <54988661+mkiszkur@users.noreply.github.com>
2024-05-12 21:17:33 -0300

    preparacion de datos

---
+++ 02_preprocess_data/geneerar_datasets.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+
+# Define la ruta al archivo principal y a la carpeta de CSVs
+ruta_principal = '/Users/miguelkiszkurno/Documents/TT1/data/resultados.csv'
+ruta_csvs = '/Users/miguelkiszkurno/Documents/TT1/data/csv'
+
+# Leer el archivo principal
+df_principal = pd.read_csv(ruta_principal)
+
+# Agrupar por 'titulo_seccion' y procesar cada grupo
+for titulo, grupo in df_principal.groupby('titulo_seccion'):
+    # DataFrame para almacenar los datos combinados de todos los indicadores en este grupo
+    df_combinado = pd.DataFrame()
+
+    # Procesar cada indicador en este grupo
+    for indicador in grupo['codigo_indicador'].unique():
+        archivo_csv = os.path.join(ruta_csvs, f"{indicador}.csv")
+        if os.path.exists(archivo_csv):
+            df_indicador = pd.read_csv(archivo_csv)
+            # Asegúrate de que el nombre de la columna de valores coincida con el nombre del archivo
+            nombre_columna_valor = indicador  # Ajusta según sea necesario
+            if nombre_columna_valor in df_indicador.columns:
+                # Concatenar horizontalmente
+                if df_combinado.empty:
+                    df_combinado = df_indicador
+                else:
+                    df_combinado = pd.merge(df_combinado, df_indicador[['Country Name','Country Code','Año', nombre_columna_valor]],
+                                            on=['Country Name','Country Code','Año'], how='outer')
+
+    # Guardar el DataFrame combinado en un nuevo CSV
+    if not df_combinado.empty:
+        df_combinado.to_csv(os.path.join(ruta_csvs, f"{titulo}.csv"), index=False)
+        print(f"Archivo guardado: {titulo}.csv")
+    else:
+        print(f"No se encontraron datos para {titulo}")
---
+++ environment/envi.md
@@ -0,0 +1,14 @@
+Para crear un archivo de requirements en función de un ambiente virtual en Python, es esencial que primero actives el entorno virtual y luego uses el comando pip freeze para generar una lista de las bibliotecas instaladas junto con sus versiones. Aquí te guío paso a paso:
+
+# Crear y activar el ambiente virtual
+Primero, si aún no tienes un ambiente virtual creado, puedes hacerlo usando venv (esto es para usuarios de sistemas basados en Unix como Linux y macOS):
+
+bash
+python3 -m venv /path/to/new/virtual/environment
+Para activar el ambiente virtual en Linux o macOS, usa:
+
+bash
+source /path/to/new/virtual/environment/bin/activate
+
+bash
+pip install -r requirements.txt
