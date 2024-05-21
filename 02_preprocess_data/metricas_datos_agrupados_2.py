26b06edddb1302a64365a8be8e3026521d034622
mkiszkur <54988661+mkiszkur@users.noreply.github.com>
2024-05-21 03:01:21 -0300

    entrega 2

---
+++ 02_preprocess_data/metricas_datos_agrupados_2.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+from datetime import datetime
+
+# Ruta a la carpeta con los archivos CSV
+folder_path = '/Users/miguelkiszkurno/Documents/TT1/data/datasets_preprocesados/sin regiones'
+
+
+# Obtener una lista de todos los archivos CSV en la carpeta
+csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+#csv_files = ['Agriculture & Rural Development.csv', 'Aid Effectiveness.csv', 'Climate Change.csv', 'Economy & Growth.csv']
+country_codes_to_include = ['ARG', 'BOL', 'BRA', 'CHL', 'COL', 'ECU', 'MEX', 'URY']
+
+# Lista para almacenar las filas del resultado
+result_data = []
+
+# Procesar cada archivo CSV
+for file_name in csv_files:
+    file_path = os.path.join(folder_path, file_name)
+    
+    # Leer el archivo CSV
+    df = pd.read_csv(file_path)
+    df = df[df['country_code'].isin(country_codes_to_include)]
+
+    # Obtener el nombre del archivo sin la extensión
+    file_base_name = os.path.splitext(file_name)[0]
+    print (f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: procesando {file_base_name}')
+
+    # Transformar el DataFrame para obtener la estructura deseada
+    for index, row in df.iterrows():
+        for indicator in df.columns[3:]:
+            result_data.append({
+                'file_name': file_base_name,
+                'country_code': row['country_code'],
+                'anho': row['anho'],
+                'indicator': indicator,
+                'is_not_null': 1 if pd.notnull(row[indicator]) else 0
+            })
+
+# Convertir la lista de diccionarios en un DataFrame
+result_df = pd.DataFrame(result_data)
+
+# Guardar el DataFrame resultante en un nuevo archivo CSV
+output_path = os.path.join(folder_path, 'combined_indicators.csv')
+result_df.to_csv(output_path, index=False)
+
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Procesamiento completado y archivo combinado guardado.')
---
+++ 02_preprocess_data/metricas_datos_country_ratios.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def country_code_sin_filtrar():
+    # Leer el archivo CSV proporcionado
+    file_path = '/Users/miguelkiszkurno/Documents/TT1/data/datasets_preprocesados/sin regiones/combined_indicators.csv'
+    df = pd.read_csv(file_path)
+
+    # Crear una tabla pivot para calcular el ratio de valores no nulos (unos) sobre el total de registros
+    pivot_table = df.pivot_table(index='file_name', columns='country_code', values='is_not_null', aggfunc='mean')
+
+    # Guardar la tabla pivot resultante en un nuevo archivo CSV
+    output_path = 'country_ratios.csv'
+    pivot_table.to_csv(output_path)
+
+    print(f"Archivo guardado en {output_path}")
+    
+
+def country_code_filtrado_por_anho():
+
+    # Leer el archivo CSV proporcionado
+    file_path = '/Users/miguelkiszkurno/Documents/TT1/data/datasets_preprocesados/sin regiones/combined_indicators.csv'
+    df = pd.read_csv(file_path)
+
+    # Filtrar los datos por los años 2015 a 2018
+    df_filtered = df[(df['anho'] >= 2015) & (df['anho'] <= 2018)]
+
+    # Crear una tabla pivot para calcular el ratio de valores no nulos (unos) sobre el total de registros
+    pivot_table = df_filtered.pivot_table(index='file_name', columns='country_code', values='is_not_null', aggfunc='mean')
+
+    # Guardar la tabla pivot resultante en un nuevo archivo CSV
+    output_path_filtered = '/Users/miguelkiszkurno/Documents/TT1/data/datasets_preprocesados/metricas/country_ratios_filtered_2015_2018.csv'
+    pivot_table.to_csv(output_path_filtered)
+
+#elijo a quien llamar en funcion de si quiero filtrar o no
+#country_code_sin_filtrar()
+country_code_filtrado_por_anho()
